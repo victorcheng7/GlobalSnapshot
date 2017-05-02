1a8f85c1b4d4e11db6f9fe5a285b02b1bd40e8e7
Thien Hoang <tonyhoang2308@gmail.com>
2017-05-02 14:22:17 -0700

    Fix bug in run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 function runTests {
-	for f in $1/*.in; do
-		filename=$(basename "$f")
-		filename="${filename%.*}"
-		./asg2 $filename setup $f > $1/$filename.out &
-	done
+    for f in $1/*.in; do
+	filename=$(basename "$f")
+	filename="${filename%.*}"
+	./asg2 $filename setup $f > $1/$filename.out &
+    done
 }
 
 function checkOutputs {
-	for f in $1/*.exp; do
-		filename=$(basename "$f")
-		filename="${filename%.*}"
-		./testOutput $f $1/$filename.out
-	done
+    for f in $1/*.exp; do
+	filename=$(basename "$f")
+	filename="${filename%.*}"
+	./testOutput $f $1/$filename.out
+    done
 }
 
 function runAllTests {
-	let num_test_cases=1
-	for d in ./tests/*; do
-		echo TESTCASE $num_test_cases:
-		runTests $d
-		while ps | grep -q asg2; do
-			sleep 1
-		done
-		checkOutputs $d
-		num_test_cases=$[num_test_cases+1]
+    let num_test_cases=1
+    for d in ./tests/*; do
+	if [[ ! -d $d ]]; then
+	    continue
+	fi
+	echo TESTCASE $num_test_cases:
+	runTests $d
+	while ps | grep -q asg2; do
+	    sleep 1
 	done
+	checkOutputs $d
+	num_test_cases=$[num_test_cases+1]
+    done
 }
 
 runAllTests
